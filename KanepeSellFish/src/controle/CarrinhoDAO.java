b68f9cab8a28b5346a6e6c2a750b3bed4ddc67a7
Elisa <vitoriaelisa114@gmail.com>
2024-10-15 11:44:18 -0300

    Criado CarrinhoDAO

---
+++ KanepeSellFish/src/controle/CarrinhoDAO.java
@@ -0,0 +1,25 @@
+package controle;
+
+import modelo.ICarrinhoDAO;
+import modelo.Pedido;
+
+public class CarrinhoDAO implements ICarrinhoDAO {
+
+	@Override
+	public boolean inserirProduto(Pedido pedido) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean alterarQuantidade(Pedido pedido) {
+
+		return false;
+	}
+
+	@Override
+	public boolean removerProduto(long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}	
+}
